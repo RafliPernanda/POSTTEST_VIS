0327dc5fa550228cf86414b62c9341f0ca677130
RafliPernanda <raflipernanda212@gmail.com>
2026-03-01 21:52:53 +0800

    postes 1 vis

---
+++ POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnTambah = New Button()
+        btnReset = New Button()
+        lblIps = New Label()
+        lblIpk = New Label()
+        txtIps = New TextBox()
+        lblPredikat = New Label()
+        lblHeader = New Label()
+        labelIPK = New Label()
+        SuspendLayout()
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(424, 217)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 0
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(535, 217)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 1
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' lblIps
+        ' 
+        lblIps.AutoSize = True
+        lblIps.Location = New Point(97, 170)
+        lblIps.Name = "lblIps"
+        lblIps.Size = New Size(247, 25)
+        lblIps.TabIndex = 2
+        lblIps.Text = "Indeks Prestasi Semester (IPS)"
+        ' 
+        ' lblIpk
+        ' 
+        lblIpk.AutoSize = True
+        lblIpk.Location = New Point(97, 281)
+        lblIpk.Name = "lblIpk"
+        lblIpk.Size = New Size(249, 25)
+        lblIpk.TabIndex = 3
+        lblIpk.Text = "Indeks Prestasi Kumulatif (IPK)"
+        ' 
+        ' txtIps
+        ' 
+        txtIps.Location = New Point(424, 170)
+        txtIps.Name = "txtIps"
+        txtIps.Size = New Size(205, 31)
+        txtIps.TabIndex = 4
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(419, 340)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(19, 25)
+        lblPredikat.TabIndex = 6
+        lblPredikat.Text = "-"
+        ' 
+        ' lblHeader
+        ' 
+        lblHeader.AutoSize = True
+        lblHeader.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblHeader.Location = New Point(97, 29)
+        lblHeader.Name = "lblHeader"
+        lblHeader.Size = New Size(611, 38)
+        lblHeader.TabIndex = 7
+        lblHeader.Text = "SISTEM AKADEMIK MUGIWARA UNIVERSITY"
+        ' 
+        ' labelIPK
+        ' 
+        labelIPK.AutoSize = True
+        labelIPK.Location = New Point(419, 289)
+        labelIPK.Name = "labelIPK"
+        labelIPK.Size = New Size(22, 25)
+        labelIPK.TabIndex = 8
+        labelIPK.Text = "0"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.GradientInactiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(labelIPK)
+        Controls.Add(lblHeader)
+        Controls.Add(lblPredikat)
+        Controls.Add(txtIps)
+        Controls.Add(lblIpk)
+        Controls.Add(lblIps)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Name = "Form1"
+        Text = "Sistem Akademik Mugiwara University"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lblIps As Label
+    Friend WithEvents lblIpk As Label
+    Friend WithEvents txtIps As TextBox
+    Friend WithEvents lblPredikat As Label
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents labelIPK As Label
+
+End Class
---
+++ POSTTEST_1/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        If Not IsNumeric(txtIps.Text) Then
+            MessageBox.Show("Masukkan angka yang valid!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+        Dim ipInput As Double = CDbl(txtIps.Text)
+
+        If ipInput < 0 Or ipInput > 4.0 Then
+            MessageBox.Show("IP Semester tidak boleh kurang dari 0 atau lebih dari 4.00!", "Input Salah", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtIps.Clear()
+            Return
+        End If
+
+        totalIP += ipInput
+        jumlahSemester += 1
+
+        Dim ipk As Double = totalIP / jumlahSemester
+        labelIPK.Text = ipk.ToString("F2")
+
+        If ipk >= 2.0 And ipk <= 2.75 Then
+            lblPredikat.Text = "Cukup"
+            lblPredikat.ForeColor = Color.Orange
+            labelIPK.ForeColor = Color.Orange
+        ElseIf ipk >= 2.76 And ipk <= 3.0 Then
+            lblPredikat.Text = "Memuaskan"
+            lblPredikat.ForeColor = Color.Green
+            labelIPK.ForeColor = Color.Green
+        ElseIf ipk >= 3.01 And ipk <= 4.0 Then
+            lblPredikat.Text = "Sangat Memuaskan"
+            lblPredikat.ForeColor = Color.Blue
+            labelIPK.ForeColor = Color.Blue
+        Else
+            lblPredikat.Text = "Tidak Memuaskan"
+            lblPredikat.ForeColor = Color.Red
+            labelIPK.ForeColor = Color.Red
+        End If
+
+        txtIps.Clear()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+        txtIps.Clear()
+        labelIPK.Text = "0.00"
+        lblPredikat.Text = "-"
+        lblPredikat.ForeColor = Color.Black
+        labelIPK.ForeColor = Color.Black
+    End Sub
+End Class
